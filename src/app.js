8aee2946d43c8a00a28df19f1bcf0e497e5202db
klofelin12 <klofelin12@bk.ru>
2018-08-01 21:31:42 +0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const config = require("config");
 
+const cors = require('cors');
 const app = express();
 
 app.use(express.static('public'));
-app.use('/api', require('./router'));
+app.use('/api', cors(), require('./router'));
 
 app.listen(config.server.port, () => {
     console.log(`app started at port ${config.server.port}`);
