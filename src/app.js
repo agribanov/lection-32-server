aed472b1a7fad79554e5a03d87ea77efd73bf6d9
Test <mihaskep@gmail.com>
2018-08-02 20:07:50 +0300

    add body parser

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const config = require("config");
 
 const cors = require('cors');
 const app = express();
 
 app.use(express.static('public'));
-app.use('/api', cors(), require('./router'));
+app.use('/api', 
+        bodyParser.json(),
+        cors(), 
+        require('./router'));
 
 app.listen(config.server.port, () => {
     console.log(`app started at port ${config.server.port}`);
